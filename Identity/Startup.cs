74c849ae004b15e78458e318e35149b5f41b6097
umutSahinkaya <49691444+UmutSahinkaya@users.noreply.github.com>
2023-01-17 01:46:55 +0300

    inception

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -11,10 +15,22 @@ namespace Identity
 {
     public class Startup
     {
+        public IConfiguration configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppIdentityDbContext>(opts =>
+            {
+                opts.UseSqlServer(configuration["ConnectionStrings:DefaultConnectionString"]);
+            });
+            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
+
+
             services.AddMvc();
         }
 
